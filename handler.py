a6f88960cd441f5e55e49fc4dc74b9bfee52710a
wfaithfull <wfaithfull@users.noreply.github.com>
2020-04-29 09:51:42 +0100

    TXMP-822 Split out redis and parsing stub function definitions

--- handler.py
+++ handler.py
@@ -7,6 +7,16 @@ from botocore.response import StreamingBody
 from s3_log_shipper.s3_event_models import S3Event
 
 
+def write_to_redis(json: dict) -> None:
+    # TODO: Write JSON to redis
+    pass
+
+
+def parse_log_line(log_line: str) -> dict:
+    # TODO: Parse log line and convert to JSON
+    pass
+
+
 def log_handler(event: dict, context) -> None:
     s3_client: BaseClient = boto3.client('s3')
     s3_event: S3Event = S3Event.from_dict(event)
@@ -23,6 +33,5 @@ def log_handler(event: dict, context) -> None:
         streaming_body: StreamingBody = get_object_response['Body']
 
         for log in gzip.GzipFile(streaming_body, mode='rb'):
-            # TODO: Parse log line and convert to JSON
-            # TODO: Write JSON to redis
-            pass
+            json_log: dict = parse_log_line(log)
+            write_to_redis(json_log)
