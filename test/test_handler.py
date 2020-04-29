d6c6c2895f7e41ea20000496d5e8a46dba75c168
wfaithfull <wfaithfull@users.noreply.github.com>
2020-04-29 09:58:30 +0100

    TXMP-822 Wrong stub here

--- test/test_handler.py
+++ test/test_handler.py
@@ -3,12 +3,12 @@ from unittest import TestCase
 from unittest.mock import Mock
 
 from handler import log_handler
-from test.fixtures import stub_record
+from test.fixtures import stub_event
 
 
 class LogHandlerSpec(TestCase):
 
     @mock_s3
     def test_log_handler_happy_path(self) -> None:
-        event: dict = stub_record('foo-bucket', 'bar-logs/ABC/DEF/baz-type-log/xyzzy.gz')
+        event: dict = stub_event('foo-bucket', 'bar-logs/ABC/DEF/baz-type-log/xyzzy.gz')
         log_handler(event=event, context=Mock())
