93206b3408020ef0d405f198abd831d0fd0d5102
wfaithfull <wfaithfull@users.noreply.github.com>
2020-04-29 17:55:57 +0100

    TXMP-822 Refactor the parsing layer and add tests

---
+++ s3_log_shipper/parsers.py
@@ -0,0 +1,73 @@
+import json
+import os
+from pathlib import Path
+from typing import Optional, List
+
+from dataclasses import dataclass
+from pygrok import Grok
+
+import s3_log_shipper
+
+
+class ParserManager:
+
+    def __init__(self, config_file: Path, groks_dir=f'{os.path.dirname(s3_log_shipper.__file__)}/groks') -> None:
+        self._config_file: Path = config_file
+
+        with open(config_file.as_posix(), 'rb') as config_file:
+            self._config: dict = json.load(config_file)
+
+        if "files" not in self.config:
+            raise ValueError("Config file format must contain top level \"files\" array")
+
+        parsers: List[Parser] = list()
+        for file in self.config['files']:
+            parser: Parser = ParserManager.make_filter(file, groks_dir)
+            parsers.append(parser)
+
+        self._filters = parsers
+
+    def __get__(self, obj, typ=None):
+        return getattr(obj, self.name)
+
+    @staticmethod
+    def make_filter(file: dict, groks_dir):
+        type: str = file['type']
+        groks = [Grok(grok, custom_patterns_dir=groks_dir) for grok in file['path']]
+        return Parser(Grok("%%{%s}" % type.upper(), custom_patterns_dir=groks_dir), groks)
+
+    @property
+    def config(self) -> dict:
+        return self._config
+
+    def get_log_path_grok_pattern(self, log_type: str) -> Optional[str]:
+        pass
+
+    def get_log_content_grok_pattern(self, log_type: str) -> str:
+        pass
+
+    def get_parser(self, log_path: str):
+        for flt in self._filters:
+            match = flt.match_path(log_path)
+            if match:
+                return flt, match
+        return None
+
+
+@dataclass
+class Parser:
+    log_grok: Grok
+    path_groks: List[Grok]
+
+    def match_path(self, text) -> Optional[dict]:
+        results = [grok.match(text) for grok in self.path_groks]
+
+        if not any(results):
+            return None
+
+        dicts = [i for i in results if i]
+
+        return {key: value for d in dicts for key, value in d.items()}
+
+    def parse_log(self, log_entry) -> Optional[dict]:
+        return self.log_grok.match(log_entry)
---
+++ test/test_parser.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+from typing import Tuple
+from unittest import TestCase
+
+from s3_log_shipper.parsers import (
+    ParserManager,
+    Parser)
+
+
+class LogParserSpec(TestCase):
+    _config_file = Path('./test_config.json').absolute()
+
+    def test_parse_no_exceptions(self):
+        parser_manager = ParserManager(self._config_file)
+        self.assertEqual(self._config_file, parser_manager._config_file)
+
+    def test_parse_path(self):
+        parser_manager = ParserManager(self._config_file)
+
+        path = "emr-logs/j-2QN8WF3UJZKK3/node/i-0c08a7e99b9985c73/applications/oozie/oozie.log-2020-04-28-05.gz"
+        prs, match = parser_manager.get_parser(path)
+
+        self.assertIsNotNone(prs)
+        self.assertIsNotNone(match)
+
+        self.assertEqual("j-2QN8WF3UJZKK3", match['cluster'])
+        self.assertEqual("i-0c08a7e99b9985c73", match['node'])
+
+    def test_parse_path_with_slash(self):
+        parser_manager = ParserManager(self._config_file)
+
+        path = "/emr-logs/j-2QN8WF3UJZKK3/node/i-0c08a7e99b9985c73/applications/oozie/oozie.log-2020-04-28-05.gz"
+        prs, match = parser_manager.get_parser(path)
+
+        self.assertIsNotNone(prs)
+        self.assertIsNotNone(match)
+
+        self.assertEqual("j-2QN8WF3UJZKK3", match['cluster'])
+        self.assertEqual("i-0c08a7e99b9985c73", match['node'])
+
+    def test_parse_path_s3(self):
+        parser_manager = ParserManager(self._config_file)
+
+        path = "s3://bucket/emr-logs/j-2QN8WF3UJZKK3/node/i-0c08a7e99b9985c73/applications/oozie/oozie.log-2020-04-28" \
+               "-05.gz "
+        prs, match = parser_manager.get_parser(path)
+
+        self.assertIsNotNone(prs)
+        self.assertIsNotNone(match)
+
+        self.assertEqual("j-2QN8WF3UJZKK3", match['cluster'])
+        self.assertEqual("i-0c08a7e99b9985c73", match['node'])
+
+    def test_parse_log(self):
+        parser_manager = ParserManager(self._config_file)
+
+        path = "emr-logs/j-2QN8WF3UJZKK3/node/i-0c08a7e99b9985c73/applications/oozie/oozie.log-2020-04-28-05.gz "
+        pm: Tuple[Parser, dict] = parser_manager.get_parser(path)
+
+        prs = pm[0]
+
+        match = prs.parse_log("2020-04-28 05:58:34,600  INFO StatusTransitService$StatusTransitRunnable:520 - SERVER["
+                              "ip-10-202- 31-224.eu-west-2.compute.internal] USER[-] GROUP[-] TOKEN[-] APP[-] JOB[-] "
+                              "ACTION[-] Released lock for [org.apache.oozie.service.StatusTransitService]")
