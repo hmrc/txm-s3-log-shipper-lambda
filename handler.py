4b2cdb6ac3ad54b257eaf9c49a41e6d4b41410f0
wfaithfull <wfaithfull@users.noreply.github.com>
2020-04-30 14:08:45 +0100

    TXMP-822 Simplify handler to use our abstractions

--- handler.py
+++ handler.py
@@ -1,37 +1,86 @@
-import gzip
+import logging
+import os
+from pathlib import Path
+from urllib.parse import (
+    ParseResult,
+    urlparse
+)
 
 import boto3
-from botocore.client import BaseClient
-from botocore.response import StreamingBody
 
+from s3_log_shipper.parsers import ParserManager
 from s3_log_shipper.s3_event_models import S3Event
+from s3_log_shipper.shipper import RedisLogShipper
+
+log: logging.Logger = logging.getLogger(__name__)
 
 
-def write_to_redis(json: dict) -> None:
-    # TODO: Write JSON to redis
-    pass
+class LazyHandlerState:
+    """
+    Stateful component which lazily creates and caches the ParserManager to keep it in memory when the lambda is in a
+    'warm' state, avoiding rereading of config files and reconstruction of parsers.
+    """
+    redis_endpoint: ParseResult
+    log_parser: ParserManager
+    shipper: RedisLogShipper
+
+    def get_log_parser(self) -> ParserManager:
+
+        if self.log_parser is None:
+            parser_config_file = Path('./grok_patterns.conf')
+
+            if not parser_config_file.exists() or not parser_config_file.is_file():
+                msg = f'Expected to find parser config file at {parser_config_file.absolute()}. No such file found.'
+                log.error(msg)
+                raise Exception(msg)
+            self.log_parser = ParserManager(config_file=parser_config_file)
+
+        return self.log_parser
+
+    @staticmethod
+    def get_redis_endpoint() -> ParseResult:
+        redis_endpoint_env_var_name = 'REDIS_ENDPOINT'
+
+        try:
+            redis_endpoint_str: str = os.environ[redis_endpoint_env_var_name]
+        except Exception as e:
+            msg = (
+                'No non-empty Redis endpoint provided to which S3 logs should be shipped. '
+                f'This should be provided as the environment variable [{redis_endpoint_env_var_name}]. '
+                f'Details: [{e}].'
+            )
+            log.error(msg)
+            raise Exception(msg)
+
+        try:
+            endpoint: ParseResult = urlparse(redis_endpoint_str)
+        except Exception as e:
+            msg = (
+                f'Expected valid Redis endpoint url. Found: [{redis_endpoint_str}]. '
+                f'Details: [{e}].'
+            )
+            log.error(msg)
+            raise Exception(msg)
+
+        return endpoint
+
+    def get_shipper(self):
+        if self.shipper is None:
+            self.shipper = RedisLogShipper(self.get_redis_endpoint(), self.get_log_parser(), boto3.client("s3"))
+
+        return self.shipper
 
 
-def parse_log_line(log_line: str) -> dict:
-    # TODO: Parse log line and convert to JSON
-    pass
+state = LazyHandlerState()
 
 
 def log_handler(event: dict, context) -> None:
-    s3_client: BaseClient = boto3.client('s3')
+    shipper = state.get_shipper()
+
     s3_event: S3Event = S3Event.from_dict(event)
 
     for record in s3_event.records:
-        bucket: str = record.s3.bucket.name
-        key: str = record.s3.object.key
+        bucket = record.s3.bucket.name
+        key = record.s3.object.key
 
-        get_object_response: dict = s3_client.get_object(Bucket=bucket, Key=key)
-
-        if 'Body' not in get_object_response:
-            raise Exception(f'Expected valid S3 get object response. Found: [{get_object_response}].')
-
-        streaming_body: StreamingBody = get_object_response['Body']
-
-        for log in gzip.GzipFile(streaming_body, mode='rb'):
-            json_log: dict = parse_log_line(log)
-            write_to_redis(json_log)
+        shipper.ship(bucket, key)
